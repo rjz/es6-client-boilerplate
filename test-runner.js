dd764660aa01e5014964e070c71ea7911a2d6558
rjz <rj@versal.com>
2015-05-30 11:57:23 -0700

    Adds es5-shim for older phantomjs installs

--- test-runner.js
+++ test-runner.js
@@ -1,4 +1,7 @@
 var context = require.context('./src', true, /_spec\.js$/);
 
+// Include `Function.prototype.bind` for older phantomjs installs
+require('es5-shim');
+
 context.keys().forEach(context);
 
